0886ac5cac5cad60639c06f3eb97117a3c0c0c11
hans.torres <https://github.com/hantorr/blockchain.git>
2019-11-22 09:26:00 -0500

    Modelo

--- BUCEF/Models/BD/Persona.cs
+++ BUCEF/Models/BD/Persona.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BUCEF.Models.BD
 {
     public class Persona
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         public string NumeroDocumento { get; set; }
         public TipoDocumento TipoDocuemto { get; set; }
